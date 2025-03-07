8b3e9cf51f767e05c414f9b4f9a0810cd9440f01
Deciptikon <deciptikon@mail.ru>
2025-03-07 22:32:03 +0300

    перестроил проект

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,15 @@
+console.log(`start app ..."`);
 import { APP_NAME } from "./utils/constants.js";
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-console.log(`start app ..."`);
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 console.log(`APP_NAME = "${APP_NAME}"`);
 if (!ctx) {
     throw new Error("Could not get 2D context");
 }
 ctx.fillStyle = "blue";
-ctx.fillRect(50, 50, 200, 100);
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 ctx.beginPath();
 ctx.arc(400, 300, 50, 0, Math.PI * 2);
 ctx.fillStyle = "red";
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
+console.log(`start app ..."`);
+
 import { PI, APP_NAME, COLORS } from "./utils/constants.js";
 
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d");
-console.log(`start app ..."`);
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
 console.log(`APP_NAME = "${APP_NAME}"`);
 
 if (!ctx) {
@@ -10,7 +15,7 @@ if (!ctx) {
 }
 
 ctx.fillStyle = "blue";
-ctx.fillRect(50, 50, 200, 100);
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 ctx.beginPath();
 ctx.arc(400, 300, 50, 0, Math.PI * 2);
